faf49ba23a5ef6eb1f5bddb3b8864a104f181518
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-04-17 15:28:52 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,188 @@
+/********************************************************
+MAKE SURE MY .end() ALLOWS FOR A SINGLE CALLBACK PARAM.
+
+SHOULD cache-service STORE/RETURN undefined WHEN KEY IS NOT FOUND?
+********************************************************/
+
+
+
+module.exports = function(superagent, config){
+
+  config = config || {};
+  var Request = superagent.Request;
+  var props = {doQuery: true, cacheWhenEmpty: true};
+  var supportedMethods = ['GET', 'PUT', 'DELETE'];
+
+  if(config.cacheService){
+    superagent.cacheService = config.cacheService;
+  }
+  else if(config.cacheServiceConfig && config.cacheModuleConfig){
+    var cs = require('cache-service');
+    superagent.cacheService = new cs(config.cacheServiceConfig, config.cacheModuleConfig);
+  }
+  else{
+    //Throw error
+  }
+
+  Request.prototype.doQuery = function(doQuery){
+    props.doQuery = doQuery;
+    return this;
+  }
+
+  Request.prototype.pruneParams = function(pruneParams){
+    props.pruneParams = pruneParams;
+    return this;
+  }
+
+  Request.prototype.pruneOptions = function(pruneOptions){
+    props.pruneOptions = pruneOptions;
+    return this;
+  }
+
+  Request.prototype.prune = function(prune){
+    props.prune = prune;
+    return this;
+  }
+
+  Request.prototype.responseProp = function(responseProp){
+    props.responseProp = responseProp;
+    return this;
+  }
+
+  Request.prototype.expiration = function(expiration){
+    props.expiration = expiration;
+    return this;
+  }
+
+  Request.prototype.cacheWhenEmpty = function(cacheWhenEmpty){
+    props.cacheWhenEmpty = cacheWhenEmpty;
+    return this;
+  }
+
+  Request.prototype._end = Request.prototype.end;
+
+  Request.prototype.end = function(cb){
+    if(~supportedMethods.indexOf(this.method)){
+      var _this = this;
+      var key = keygen(this);
+
+      if(this.method === 'GET'){
+      	superagent.cacheService.getKey(key, function (err, response){
+	        if(!err && response){
+	        	resetProps();
+	          cb(err, response, key);
+	        }
+	        else{
+	          if(props.doQuery){
+	            _this._end(function (err, response){
+	            	if(!err && response){
+	            		response = gutResponse(response);
+		              if(props.prune){
+		                response = props.prune(response);
+		              }
+		              else if(props.responseProp){
+		                response = response[props.responseProp] || null;
+		              }
+		              if(!isEmpty(response) || props.cacheWhenEmpty){
+		                superagent.cacheService.setKey(key, response, props.expiration, function(){
+		                  resetProps();
+		                  cb(err, response, key);
+		                });
+		              }
+		              else{
+		              	resetProps();
+		                cb(err, response, key);
+		              }
+	            	}
+	            });
+	          }
+	          else{
+	          	resetProps();
+	          	cb(null, null, key);
+	          }
+	        }
+	      });
+      }
+      else{
+      	this._end(function (err, response){
+      		resetProps();
+      		if(!err && response){
+      			resetProps();
+      			cb(err, response, key);
+      			superagent.cacheService.deleteKeys(key, noop);
+      		}
+      	});
+      }
+    }
+    else{
+      this._end(function (err, response){
+        resetProps();
+        cb(err, response, undefined);
+      });
+    }
+  }
+
+  Request.prototype.reset = function(){
+    this.req = null;
+  }
+
+  function keygen(req){
+    var cleanParams = null;
+    var cleanOptions = null;
+    var options = (req.req && req.req._headers) ? req.req._headers : {};
+    if(props.pruneParams || props.pruneOptions){
+      cleanParams = (props.pruneParams) ? pruneObj(cloneObject(req.qs), props.pruneParams) : req.qs;
+      cleanOptions = (props.pruneOptions) ? pruneObj(cloneObject(options), props.pruneOptions, true) : options;
+    }
+    return JSON.stringify({
+      nameSpace: superagent.cacheService.nameSpace,
+      method: req.method,
+      uri: req.url,
+      params: cleanParams || req.qs,
+      options: cleanOptions || options,
+    });
+  }
+
+  function pruneObj(obj, props, isOptions){
+  	for(var i = 0; i < props.length; i++){
+  		var prop = props[i];
+  		if(isOptions){
+  			prop = prop.toLowerCase();
+  		}
+  		delete obj[prop]
+  	}
+  	return obj;
+  }
+
+  function gutResponse(r){
+    var newResponse = {};
+    newResponse.body = r.body;
+    newResponse.text = r.text;
+    newResponse.headers = r.headers;
+    newResponse.statusCode = r.statusCode;
+    newResponse.status = r.status;
+    newResponse.ok = r.ok;
+    return newResponse;
+  }
+
+  function isEmpty(val){
+    return (val === false || val === null || (typeof val == 'object' && Object.keys(val).length == 0));
+  }
+
+  function cloneObject(obj){
+    var newObj = {};
+    for(var attr in obj) {
+      if (obj.hasOwnProperty(attr)){
+        newObj[attr] = obj[attr];
+      }
+    }
+    return newObj;
+  }
+
+  function resetProps(){
+    props = {doQuery: true, cacheWhenEmpty: true};
+  }
+
+  var noop = function(){}
+
+}
---
+++ test/server/superagent-cache.js
@@ -0,0 +1,281 @@
+var expect = require('expect');
+var express = require('express');
+var superagent = require('superagent');
+var cs = require('cache-service').cacheService;
+var cacheService = new cs();
+require('../../')(superagent, {cacheService: cacheService});
+ 
+var app = express();
+ 
+app.get('/one', function(req, res){
+  res.send(200, {key: 'one'});
+});
+
+app.post('/one', function(req, res){
+  res.send(200, {key: 'post'});
+});
+
+app.put('/one', function(req, res){
+  res.send(200, {key: 'put'});
+});
+
+app.delete('/one', function(req, res){
+  res.send(200, {key: 'delete'});
+});
+
+app.get('/false', function(req, res){
+  res.send(200, {key: false});
+});
+
+app.get('/params', function(req, res){
+  res.send(200, {pruneParams: req.query.pruneParams, otherParams: req.query.otherParams});
+});
+
+app.get('/options', function(req, res){
+  res.send(200, {pruneOptions: req.get('pruneOptions'), otherOptions: req.get('otherOptions')});
+});
+
+app.listen(3000);
+
+/*
+
+Custom API items to test:
+
+  TEST THAT I CAN PASS IN BOTH A cache-service INSTANCE AS WELL AS A CONFIG THAT WILL BECOME A cache-service INSTANCE
+
+  cache-service level api
+    chainable
+      superagent functions
+        .get() should cache                                                         done
+        .post() should not cache                                                    done
+        .put() should invalidate cache                                              done
+        .del() should invalidate cache                                              done
+      custom
+        ._end() should not cache                                                    done
+        .responseProp() should chop response                                        done
+        .prune() should prune response                                              done
+        .pruneParams() should generate a cache key without certain params           done
+        .pruneOptions() should generate a cache key without certain options         done
+        .expiration() should override all caches' defaultExpirations                done
+        .cacheWhenEmpty() should cache when response is empty                       done
+        .doQuery() should allow users to skip querying but still check all caches   done
+
+*/
+
+describe('Array', function(){
+
+  beforeEach(function(){
+    cacheService.flushKeys();
+  });
+
+  describe('superagentCache API tests', function () {
+
+    it('.get() .prune() .end() should prune response before caching', function (done) {
+      var prune = function(r){
+        return (r && r.ok && r.body) ? r.body.key : null;
+      }
+      superagent
+        .get('localhost:3000/one')
+        .prune(prune)
+        .end(function (err, response, key){
+          expect(response).toBe('one');
+          done();
+        }
+      );
+    });
+
+    it('.get() .responseProp() .end() should get responseProp before caching', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .responseProp('body')
+        .end(function (err, response, key){
+          expect(response.key).toBe('one');
+          done();
+        }
+      );
+    });
+
+    it('.get() .expiration() .end() should override all caches\' defaultExpirations', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .expiration(0.001)
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('one');
+          superagent.cacheService.getKey(key, function (err, result){
+            expect(result.body.key).toBe('one');
+          });
+          setTimeout(function(){
+            superagent.cacheService.getKey(key, function (err, result){
+              expect(result).toBe(null);
+              done();
+            });
+          }, 10);
+        }
+      );
+    });
+
+    it('.get() .prune() .end() should cache an empty response', function (done) {
+      var prune = function(r){
+        return (r && r.ok && r.body) ? r.body.key : null;
+      }
+      superagent
+        .get('localhost:3000/false')
+        .prune(prune)
+        .end(function (err, response, key){
+          expect(response).toBe(false);
+          superagent.cacheService.getKey(key, function (err, response){
+            expect(response).toBe(false);
+            done();
+          });
+        }
+      );
+    });
+
+    it('.get() .prune() .cacheWhenEmpty(false) .end() should not cache an empty response', function (done) {
+      var prune = function(r){
+        return (r && r.ok && r.body) ? r.body.key : null;
+      }
+      superagent
+        .get('localhost:3000/false')
+        .prune(prune)
+        .cacheWhenEmpty(false)
+        .end(function (err, response, key){
+          expect(response).toBe(false);
+          superagent.cacheService.getKey(key, function (err, response){
+            expect(response).toBe(null);
+            done();
+          });
+        }
+      );
+    });
+
+    it('.get() .pruneParams() .end() should query with all params but create a key without the indicated params', function (done) {
+      superagent
+        .get('localhost:3000/params')
+        .query({pruneParams: true, otherParams: false})
+        .pruneParams(['pruneParams'])
+        .end(function (err, response, key){
+          expect(response.body.pruneParams).toBe('true');
+          expect(response.body.otherParams).toBe('false');
+          expect(key.indexOf('pruneParams')).toBe(-1);
+          expect(key.indexOf('otherParams')).toBeGreaterThan(-1);
+          done();
+        }
+      )
+    });
+
+    it('.get() .pruneOptions() .end() should query with all options but create a key without the indicated options', function (done) {
+      superagent
+        .get('localhost:3000/options')
+        .set({pruneOptions: true, otherOptions: false})
+        .pruneOptions(['pruneOptions'])
+        .end(function (err, response, key){
+          expect(response.body.pruneOptions).toBe('true');
+          expect(response.body.otherOptions).toBe('false');
+          //Superagent converts headers to lower case so I check here for lower case versions of the headers sent above
+          expect(key.indexOf('pruneoptions')).toBe(-1);
+          expect(key.indexOf('otheroptions')).toBeGreaterThan(-1);
+          done();
+        }
+      )
+    });
+
+    it('.get() .doQuery(false) .end() should not perform a query', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .doQuery(false)
+        .end(function (err, response, key){
+          expect(response).toBe(null);
+          done();
+        }
+      );
+    });
+
+  });
+
+  describe('superagentCache caching tests', function () {
+    
+    it('.get() ._end() should bypass all caching logic', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        ._end(function (err, response, key){
+          expect(typeof key).toBe('undefined');
+          expect(response.body.key).toBe('one');
+          done();
+        }
+      );
+    });
+
+    it('.post() .end() should bypass all caching logic', function (done) {
+      superagent
+        .post('localhost:3000/one')
+        .end(function (err, response, key){
+          expect(typeof key).toBe('undefined');
+          expect(response.body.key).toBe('post');
+          done();
+        }
+      );
+    });
+
+    it('.get() .end() should retrieve and cache response', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('one');
+          superagent.cacheService.getKey(key, function (err, response){
+            expect(response.body.key).toBe('one');
+            done();
+          });
+        }
+      );
+    });
+
+    it('.put() .end() should invalidate the generated cache key', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('one');
+          superagent.cacheService.getKey(key, function (err, response){
+            expect(response.body.key).toBe('one');
+            superagent
+              .put('localhost:3000/one')
+              .end(function (err, response, key){
+                expect(typeof key).toBe('string');
+                expect(response.body.key).toBe('put');
+                superagent.cacheService.getKey(key, function (err, response){
+                  expect(response).toBe(null);
+                  done();
+                });
+              }
+            );
+          });
+        }
+      );
+    });
+
+    it('.del() .end() should invalidate the generated cache key', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('one');
+          superagent.cacheService.getKey(key, function (err, response){
+            expect(response.body.key).toBe('one');
+            superagent
+              .put('localhost:3000/one')
+              .end(function (err, response, key){
+                expect(typeof key).toBe('string');
+                expect(response.body.key).toBe('put');
+                superagent.cacheService.getKey(key, function (err, response){
+                  expect(response).toBe(null);
+                  done();
+                });
+              }
+            );
+          });
+        }
+      );
+    });
+
+  });
+
+});
